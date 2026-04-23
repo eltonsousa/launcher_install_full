9318f08a695533051ed2c205762cda0a0e0de4d7
Elton Sousa <eltonsousadesigner@gmail.com>
2026-04-23 17:42:19 -0400

    Pronto para distribuição [OK]

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // config.js - Central de Configurações
 const config = {
-  gameName: "Talisman Teste",
-  serverIp: "192.168.1.144",
-  registerUrl: "http://192.168.1.144",
-  patchUrl: "http://192.168.1.144/patch/patch.json",
+  gameName: "Talisman Origem",
+  serverIp: "144.217.210.249",
+  registerUrl: "http://144.217.210.249",
+  patchUrl: "http://144.217.210.249/patch/patch.json",
 };
 
 module.exports = config;
